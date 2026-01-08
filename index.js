2f150b1813d88c34d0f870908793c9c18f461b1b
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-08 23:30:11 +0100

    Refactor fetch event listener to async fetch method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-  const path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname;
-
-  event.respondWith(fetch(path, event.request));
-});
+// index.js
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    // Ha root, akkor a főoldalt küldjük
+    const path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname;
+    return fetch(path, request);
+  }
+};
