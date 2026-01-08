e5cd35bb5fa954e4f8c7f363e9ccccc3ad13807b
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-08 23:12:33 +0100

    Refactor fetch event handling and add handleRequest

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event));
+});
 
-addEventListener('fetch', event => {
+async function handleRequest(event) {
+  const url = new URL(event.request.url);
+  let path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname; // kezdőlap beállítása
+
   try {
-    event.respondWith(
-      getAssetFromKV(event)
-    )
-  } catch (e) {
-    event.respondWith(
-      new Response('Page not found', { status: 404 })
-    )
+    return await fetch(new Request(`./direct${path}`, event.request));
+  } catch (err) {
+    return new Response("File not found", { status: 404 });
   }
-})
+}
