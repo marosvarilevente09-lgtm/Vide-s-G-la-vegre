af3479ad6a13ce9f6912d8fa3d43b82662884a7c
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-08 23:27:38 +0100

    Simplify fetch event handling in index.js
    Refactor fetch event listener to simplify request handling.

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event));
-});
-
-async function handleRequest(event) {
   const url = new URL(event.request.url);
-  let path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname;
+  const path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname;
 
-  try {
-    return await fetch(new Request(path, event.request));
-  } catch (err) {
-    return new Response("File not found", { status: 404 });
-  }
-}
+  event.respondWith(fetch(path, event.request));
+});
