f57ce1a57fd09c2e1747c31cfa616f25194ab3e6
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-09 12:54:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ addEventListener('fetch', event => {
 async function handleRequest(event) {
   try {
     const url = new URL(event.request.url);
-
-    // Ha a főoldalra mennél, mindig index.html-t adunk
     const path = url.pathname === '/' ? '/index.html' : url.pathname;
 
-    // getAssetFromKV mindig friss fájlt ad, nem cache-elt
     return await getAssetFromKV(event, {
       mapRequestToAsset: req => new Request(path, req),
       cacheControl: {
-        bypassCache: true  // 🟢 Ez a kulcs, mindig friss
+        bypassCache: true  // 🟢 Mindig frissíti a CSS-t és HTML-t
       }
     });
   } catch (err) {
