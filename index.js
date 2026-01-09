3290a15e2965b8b59ec1ef8e790596a7ef80436e
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-09 11:34:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-addEventListener("fetch", event => {
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+addEventListener('fetch', event => {
   event.respondWith(handleRequest(event));
 });
 
 async function handleRequest(event) {
-  const url = new URL(event.request.url);
-  // "/" esetén index.html
-  let path = url.pathname === "/" ? "/index.html" : url.pathname;
-
   try {
-    return await fetch(new Request(path, event.request));
+    // Ha a felhasználó a főoldalra megy, akkor index.html-t adunk
+    const url = new URL(event.request.url);
+    const path = url.pathname === '/' ? '/index.html' : url.pathname;
+
+    // Cloudflare assets handler-rel próbáljuk kiszolgálni
+    return await getAssetFromKV(event, { mapRequestToAsset: req => new Request(path, req) });
   } catch (err) {
-    return new Response("File not found", { status: 404 });
+    return new Response('File not found', { status: 404 });
   }
 }
