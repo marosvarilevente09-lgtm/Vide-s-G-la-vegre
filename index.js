9c41a4f380fcf1ffd32813fb3320377dcaeda853
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-09 11:17:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event));
-});
-
-async function handleRequest(event) {
-  const url = new URL(event.request.url);
-  let path = url.pathname === "/" ? "/Szavazas 2026.html" : url.pathname;
-
-  try {
-    return await fetch(new Request(`${path}`, event.request));
-  } catch (err) {
-    return new Response("File not found", { status: 404 });
+export default {
+  fetch(request, env, ctx) {
+    return env.ASSETS.fetch(request);
   }
-}
+};
