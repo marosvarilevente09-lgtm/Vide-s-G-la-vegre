a7f8a1b7d07923aefb53c1e6d58e9b06a38636dc
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-09 11:35:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ addEventListener('fetch', event => {
 
 async function handleRequest(event) {
   try {
-    // Ha a felhasználó a főoldalra megy, akkor index.html-t adunk
     const url = new URL(event.request.url);
+    // Ha a főoldalra megyünk, index.html-t adunk vissza
     const path = url.pathname === '/' ? '/index.html' : url.pathname;
 
-    // Cloudflare assets handler-rel próbáljuk kiszolgálni
-    return await getAssetFromKV(event, { mapRequestToAsset: req => new Request(path, req) });
+    // Kiszolgáljuk a statikus fájlokat a 'direct' mappából
+    return await getAssetFromKV(event, {
+      mapRequestToAsset: req => new Request(path, req)
+    });
   } catch (err) {
     return new Response('File not found', { status: 404 });
   }
