6ee1ac7a9fc4e1f3d37c6b846491d500271e9611
marosvarilevente09-lgtm <marosvarilevente09@gmail.com>
2026-01-09 12:15:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ addEventListener('fetch', event => {
 
 async function handleEvent(event) {
   try {
-    const url = new URL(event.request.url);
-    // ha a kezdőlap, mindig az Szavazas 2026.html-t adja
     const options = {
       mapRequestToAsset: req => {
         let pathname = new URL(req.url).pathname;
-        if (pathname === "/") pathname = "/Szavazas 2026.html";
+        if (pathname === "/") pathname = "/index.html"; // kezdőlap
         return new Request(pathname, req);
       }
     };
